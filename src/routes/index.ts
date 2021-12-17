26ee14596e4d8b15c55894924af91fa255030ae6
Rodrigo Silvério <rodrigo.silverio@fengbrasil.com.br>
2021-12-16 22:29:41 -0300

    feat(Movie): Added CRUD

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import categoryRoutes from './category.routes';
+import movieRoutes from './movie.routes';
 
 const routes = Router();
 
 routes.use('/categories', categoryRoutes);
+routes.use('/movies', movieRoutes);
 
 export default routes;
